7898f24939b677fcd100ab4039d01a083656acbe
Eduard <eduardolteanu@gmail.com>
2022-10-31 01:17:04 +0200

    Functie de returnat un fisier txt

---
+++ mk.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+
+var file = path.join(__dirname, 'test.txt');
+
+fs.writeFile(file, 'hello world', function(err) {
+    if (err) throw err;
+    console.log('File saved!');
+    }
+);
+
