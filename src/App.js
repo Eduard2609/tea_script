8b29c45f43f30db7af792d175bd07efb35fc2956
Eduard <eduardolteanu@gmail.com>
2022-11-12 22:59:42 +0200

    sidebard first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import React from 'react'; 
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
+  const activeMenu = 'ture';
   return (
-    <h1 className='underline text-3x1'>TeaScript</h1>
+    // test buton login
+    // <div>
+    //   {/* <h1 className='underline text-3x1'>TeaScript</h1> */}
+    //   <BrowserRouter>
+    //     <div className="flex relative dark:bg-main-dark-bg">
+    //       <div 
+    //       className="fixed left-4 top-4"
+    //       style={{ zIndex: '1000'}}>
+    //         <TooltipComponent
+    //           content="Login/Signup" position='BottomCenter'>
+    //           <button type="button" className="text-6x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray text-blue" style={{bakcground: 'blue', borderRadius: '50%'}}>
+    //           Login/Signup 
+    //           </button>
+    //         </TooltipComponent>
+    //       </div>
+    //     </div> 
+    //   </BrowserRouter>
+    // </div>
+    <div> 
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+            <TooltipComponent 
+              content="Settings" position='Top'>
+              <button type="button" className="text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray text-black" style={{bakcground: 'green', borderRadius: '50%'}}>
+                Settings
+              </button>
+            </TooltipComponent>
+            </div>
+            {activeMenu ? (
+              <div>
+                Sidebard
+              </div>
+            ) : (
+              <div>
+                Sidebard w-0
+              </div>
+                )}
+          </div>
+        </BrowserRouter>
+      </div> 
+
   );
 }
 
